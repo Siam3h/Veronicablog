4b604794cd97f426c8b75a36a5b746381b105312
siam3h <philosiama@gmail.com>
2024-09-25 21:40:01 +0300

    no change or add transaction in admin interface

--- payments/admin.py
+++ payments/admin.py
@@ -11,3 +11,9 @@ class TransactionAdmin(admin.ModelAdmin):
     list_display = ('email', 'project', 'amount', 'verified')
     search_fields = ('email', 'project__title')
     list_filter = ('verified',)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
